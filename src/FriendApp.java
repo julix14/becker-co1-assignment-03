a3b4d48027b8431d52e28519e0d33e0b77f8cae2
juliusbecker <julix.becker@gmail.com>
2023-01-08 14:22:31 +0100

    Add full functionality to programm

--- src/FriendApp.java
+++ src/FriendApp.java
@@ -1,7 +1,13 @@
+import dataservices.FriendListExportService;
+import gui.GuiController;
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.FocusModel;
 import javafx.stage.Stage;
 
 import java.net.URL;
@@ -16,13 +22,20 @@ public class FriendApp extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        URL urlToLayout = getClass().getResource("guidata/Layout.fxml");
 
-        assert urlToLayout != null;
-        Parent root = FXMLLoader.load(urlToLayout);
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("guidata/Layout.fxml"));
+        Parent root = loader.load();
 
         stage.setTitle("Friends-Organizer");
         stage.setScene(new Scene(root));
         stage.show();
+
+        GuiController controller = loader.getController();
+
+        stage.setOnCloseRequest(event -> {
+            controller.shutdown();
+        });
+
+
     }
 }
--- src/gui/GuiController.java
+++ src/gui/GuiController.java
@@ -1,40 +1,104 @@
 package gui;
 
 import dataclasses.Friend;
+import dataservices.FriendListExportService;
 import dataservices.FriendListImportService;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class GuiController implements Initializable {
     @FXML
     private ListView<String> friendsListView;
 
     @FXML
-    private Label friendName;
+    private AnchorPane baseScene;
 
     @FXML
-    private Label friendPhoneNumber;
+    private TextField selectedFriendName;
 
-    List<Friend> friendList;
+    @FXML
+    private TextField selectedFriendNumber;
+
+    @FXML
+    private TextField newFriendName;
+
+    @FXML
+    private TextField newFriendNumber;
+
+    @FXML
+    private Button addNewFriend;
+
+    @FXML
+    private Button deleteFriend;
+
+    @FXML
+    private Button editFriend;
+
+    @FXML
+    private TextField search;
+
+    @FXML
+    private Button searchButton;
+
+    public List<Friend> friendList;
     Friend currentFriend;
 
     @Override
     public void initialize(java.net.URL location, java.util.ResourceBundle resources) {
         friendList = new FriendListImportService().importFriends();
-        List<String> friendNames = friendList.stream().map(Friend::getName).toList();
+        AtomicReference<List<String>> friendNames = new AtomicReference<>(friendList.stream().map(Friend::getName).toList());
 
-        friendsListView.getItems().addAll(friendNames);
+
+        friendsListView.getItems().addAll(friendNames.get());
 
         friendsListView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
             currentFriend = friendList.stream().filter(friend -> friend.getName().equals(newValue)).findFirst().get();
-            friendName.setText(currentFriend.getName());
-            friendPhoneNumber.setText(currentFriend.getPhoneNumber());
+            selectedFriendName.setText(currentFriend.getName());
+            selectedFriendNumber.setText(currentFriend.getPhoneNumber());
         });
 
+        searchButton.setOnAction(event -> {
+            friendsListView.getItems().clear();
+            friendNames.set(friendList.stream().filter(friend -> friend.getName().toLowerCase().contains(search.getText().toLowerCase())).map(Friend::getName).toList());
+            friendsListView.getItems().addAll(friendNames.get());
+        });
 
+        addNewFriend.setOnAction(event -> {
+            Friend newFriend = new Friend();
+            newFriend.setName(newFriendName.getText());
+            newFriend.setPhoneNumber(newFriendNumber.getText());
+            friendList.add(newFriend);
+            friendsListView.getItems().add(newFriend.getName());
+        });
+
+        deleteFriend.setOnAction(event -> {
+            friendList.remove(currentFriend);
+            friendsListView.getItems().remove(currentFriend.getName());
+        });
+
+        editFriend.setOnAction(event -> {
+            currentFriend.setName(selectedFriendName.getText());
+            currentFriend.setPhoneNumber(selectedFriendNumber.getText());
+            friendsListView.getItems().set(friendsListView.getSelectionModel().getSelectedIndex(), currentFriend.getName());
+        });
+
+    }
+
+    public void shutdown() {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Do you want to save your changes?", ButtonType.YES, ButtonType.NO);
+
+        if (alert.showAndWait().get() == ButtonType.YES) {
+            new FriendListExportService().exportFriends(friendList);
+            System.out.println("Saved");
+        }else {
+            System.out.println("Changes not saved");
+        }
     }
 }
